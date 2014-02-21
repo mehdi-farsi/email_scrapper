c91ce365a46019604d0f819449984b840cbd8eb2
mehdi <mehdi.farsi@epitech.eu>
2014-02-21 16:25:55 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmailScrapping::Application.config.secret_key_base = '3f95f8bb57d4580a6bae66be610629e551c721e403b1dd9ebeb8a7dfa89a8563b9307df0020ffe7553d35f36897584e5a616cbcdde0a9314f83bb8f314c99d5d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmailScrapping::Application.config.session_store :cookie_store, key: '_email_scrapping_session'
